8eacb3a661d55a9340702fcec6375b94057eb6e8
Arielle Chen & erictflores <pair+achen116.erictflores@devbootcamp.com>
2015-07-18 11:33:18 -0700

    add error handling for geolocation and initial react file

---
+++ frontend/js/enable_location.js
@@ -0,0 +1,9 @@
+var EnableLocation = React.createClass({
+  render: function() {
+    return (
+      <div>Enable Location Working!</div>
+    );
+  }
+});
+
+React.render(<EnableLocation />, document.getElementById('container'));
--- frontend/js/main.js
+++ frontend/js/main.js
@@ -4,11 +4,11 @@ $(document).on("ready", function() {
 
   function getLocation() {
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(showPosition);
+      navigator.geolocation.getCurrentPosition(showPosition, showError);
     } else {
       x.innerHTML = "Geolocation is not supported by this browser.";
     }
-  }
+  } // ends getLocation function
 
   function showPosition(position) {
 
@@ -20,7 +20,6 @@ $(document).on("ready", function() {
     });
 
     request.done(function(response) {
-      console.log(response);
       for (var i = 0; i < response.length; i ++) {
         $('body').append("<p style='font-weight:bold;'>" + response[i].hash.name + " <span style='font-weight:normal;font-style:italic;color: red'>" + response[i].hash.rating + "</span></p>");
       }
@@ -29,10 +28,27 @@ $(document).on("ready", function() {
     request.fail(function(errors) {
       console.error(errors);
     });
-    
+
     x.innerHTML = "Latitude: " + position.coords.latitude +
     "<br>Longitude: " + position.coords.longitude;
-  }
+  } // ends showPosition function
+
+  function showError(error) {
+    switch(error.code) {
+      case error.PERMISSION_DENIED:
+        x.innerHTML = "User denied the request for Geolocation."
+        break;
+      case error.POSITION_UNAVAILABLE:
+        x.innerHTML = "Location information is unavailable."
+        break;
+      case error.TIMEOUT:
+        x.innerHTML = "The request to get user location timed out."
+        break;
+      case error.UNKNOWN_ERROR:
+        x.innerHTML = "An unknown error occurred."
+        break;
+    } // ends switch
+  } // ends showError function
 
   getLocation();
 
