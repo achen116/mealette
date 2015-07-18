955fcb1d1c8c1e34d8fd07744e19e48c95a93717
Arielle Chen & erictflores <pair+achen116.erictflores@devbootcamp.com>
2015-07-18 12:25:28 -0700

    begin working on enable location react component - lunch break

--- frontend/js/enable_location.js
+++ frontend/js/enable_location.js
@@ -1,7 +1,63 @@
 var EnableLocation = React.createClass({
+  componentDidMount: function() {
+    var x = document.getElementById("demo");
+    debugger
+    this.getLocation();
+  },
+
+  getLocation: function() {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(this.showPosition, this.showError);
+      } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+      }
+  },
+
+  showPosition: function(position) {
+      var request = $.ajax({
+        url: "http://localhost:3000/api",
+        method: "get",
+        data: {lat: position.coords.latitude, lon: position.coords.longitude},
+        dataType: "JSON"
+      });
+
+      request.done(function(response) {
+        for (var i = 0; i < response.length; i ++) {
+          $('body').append("<p style='font-weight:bold;'>" + response[i].hash.name + " <span style='font-weight:normal;font-style:italic;color: red'>" + response[i].hash.rating + "</span></p>");
+        }
+      });
+
+      request.fail(function(errors) {
+        console.error(errors);
+      });
+
+      x.innerHTML = "Latitude: " + position.coords.latitude +
+      "<br>Longitude: " + position.coords.longitude;
+  },
+
+  showError: function(error) {
+      switch(error.code) {
+        case error.PERMISSION_DENIED:
+          x.innerHTML = "User denied the request for Geolocation."
+          break;
+        case error.POSITION_UNAVAILABLE:
+          x.innerHTML = "Location information is unavailable."
+          break;
+        case error.TIMEOUT:
+          x.innerHTML = "The request to get user location timed out."
+          break;
+        case error.UNKNOWN_ERROR:
+          x.innerHTML = "An unknown error occurred."
+          break;
+      }
+  },
+
   render: function() {
     return (
-      <div>Enable Location Working!</div>
+      <div>
+        <div>Enable Location Working!</div>
+        <div>{this.props.message}</div>
+      </div>
     );
   }
 });
--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,55 +1,55 @@
 $(document).on("ready", function() {
 
-  var x = document.getElementById("demo");
+  // var x = document.getElementById("demo");
 
-  function getLocation() {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(showPosition, showError);
-    } else {
-      x.innerHTML = "Geolocation is not supported by this browser.";
-    }
-  } // ends getLocation function
+  // function getLocation() {
+  //   if (navigator.geolocation) {
+  //     navigator.geolocation.getCurrentPosition(showPosition, showError);
+  //   } else {
+  //     x.innerHTML = "Geolocation is not supported by this browser.";
+  //   }
+  // } // ends getLocation function
 
-  function showPosition(position) {
+  // function showPosition(position) {
 
-    var request = $.ajax({
-      url: "http://localhost:3000/api",
-      method: "get",
-      data: {lat: position.coords.latitude, lon: position.coords.longitude},
-      dataType: "JSON"
-    });
+  //   var request = $.ajax({
+  //     url: "http://localhost:3000/api",
+  //     method: "get",
+  //     data: {lat: position.coords.latitude, lon: position.coords.longitude},
+  //     dataType: "JSON"
+  //   });
 
-    request.done(function(response) {
-      for (var i = 0; i < response.length; i ++) {
-        $('body').append("<p style='font-weight:bold;'>" + response[i].hash.name + " <span style='font-weight:normal;font-style:italic;color: red'>" + response[i].hash.rating + "</span></p>");
-      }
-    });
+  //   request.done(function(response) {
+  //     for (var i = 0; i < response.length; i ++) {
+  //       $('body').append("<p style='font-weight:bold;'>" + response[i].hash.name + " <span style='font-weight:normal;font-style:italic;color: red'>" + response[i].hash.rating + "</span></p>");
+  //     }
+  //   });
 
-    request.fail(function(errors) {
-      console.error(errors);
-    });
+  //   request.fail(function(errors) {
+  //     console.error(errors);
+  //   });
 
-    x.innerHTML = "Latitude: " + position.coords.latitude +
-    "<br>Longitude: " + position.coords.longitude;
-  } // ends showPosition function
+  //   x.innerHTML = "Latitude: " + position.coords.latitude +
+  //   "<br>Longitude: " + position.coords.longitude;
+  // } // ends showPosition function
 
-  function showError(error) {
-    switch(error.code) {
-      case error.PERMISSION_DENIED:
-        x.innerHTML = "User denied the request for Geolocation."
-        break;
-      case error.POSITION_UNAVAILABLE:
-        x.innerHTML = "Location information is unavailable."
-        break;
-      case error.TIMEOUT:
-        x.innerHTML = "The request to get user location timed out."
-        break;
-      case error.UNKNOWN_ERROR:
-        x.innerHTML = "An unknown error occurred."
-        break;
-    } // ends switch
-  } // ends showError function
+  // function showError(error) {
+  //   switch(error.code) {
+  //     case error.PERMISSION_DENIED:
+  //       x.innerHTML = "User denied the request for Geolocation."
+  //       break;
+  //     case error.POSITION_UNAVAILABLE:
+  //       x.innerHTML = "Location information is unavailable."
+  //       break;
+  //     case error.TIMEOUT:
+  //       x.innerHTML = "The request to get user location timed out."
+  //       break;
+  //     case error.UNKNOWN_ERROR:
+  //       x.innerHTML = "An unknown error occurred."
+  //       break;
+  //   } // ends switch
+  // } // ends showError function
 
-  getLocation();
+  // getLocation();
 
 });
